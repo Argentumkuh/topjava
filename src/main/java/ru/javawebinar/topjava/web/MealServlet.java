ca329b9a3ff8b539f6b667612999f0583cad9c9a
Argentumkuh <Argentumkuh@gmail.com>
2017-11-08 22:20:54 +0200

    MealServlet added2

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @WebServlet(name = "MealServlet")
 public class MealServlet extends HttpServlet {
+
+    private static final Logger log = getLogger(MealServlet.class);
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meal");
 
+        request.getRequestDispatcher("/meal.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/meal.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Argentum_kuh
+  Date: 08.11.2017
+  Time: 22:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meal</title>
+</head>
+<body>
+<h3><a href="index.html">Home</a></h3>
+<h2>Meal</h2>
+</body>
+</html>
